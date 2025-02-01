e22ee87a4a59dc0ffd75289c7b597321cf1d671b
Rabbani978 <rabbani600978@gmail.com>
2025-02-01 12:41:04 +0600

    again solved

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-build:{
-    rollupoptions: {
-        input: {
-            main: resolve(_dirname, "index.html"),
-            news: resolve(_dirname, "news.html"),
-            },
-        },
+  build: {
+    rollupOptions: {  // Corrected property name
+      input: {
+        main: resolve(__dirname, "index.html"),  // Corrected variable name
+        news: resolve(__dirname, "news.html"),
+      },
     },
+  },
 });
